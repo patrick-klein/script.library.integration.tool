21f212d590af868ccf038b25f5a199eb1c14a409
luizoti <luizlazoti@hotmail.com>
2021-01-19 00:02:04 -0300

    1. returning to the container_type method, ListItem.TVShowTitle, ListItem.Title does not work well 2. added a new window dialog to ask user to choide type of content

--- context.py
+++ context.py
@@ -13,44 +13,51 @@ import xbmcgui
 import resources.lib.utils as utils
 from resources.lib.menus.synced import SyncedMenu
 
+def label_with_year(title, year):
+    # Amazon dont return year for all items, year will be added only if exist
+    if len(year) == 4:
+        return ('%s (%s)' % (title, year))
+    else:
+        return ('%s' % title)
+        pass
 
 @utils.entrypoint
 def main():
     ''' Main entrypoint for context menu item '''
-    # container_type = xbmc.getInfoLabel('Container.Content')
-    # label = sys.listitem.getLabel()  # pylint: disable=E1101
+    label = sys.listitem.getLabel()  # pylint: disable=E1101
     path = sys.listitem.getPath()  # pylint: disable=E1101
     # 
     year = xbmc.getInfoLabel('ListItem.Year')
-    # 
-    # this method works, if is a show or a movie will be not empty
-    tvshowtitle = xbmc.getInfoLabel('ListItem.TVShowTitle')
-    movietitle = xbmc.getInfoLabel('ListItem.Title')
+    container_type = xbmc.getInfoLabel('Container.Content')
 
     # Get content type
     # ATENTION: the inclusion of year is a test, Netflix and Amazon VOD works in tests to give year for shows and movies
     # but can not work in all contents
-    if len(tvshowtitle) > 0:
-        content_type = 'tvshow'
-        label = ('%s (%s)' % (tvshowtitle, year))
-    elif len(movietitle) > 0:
-        content_type = 'movie'
-        label = ('%s (%s)' % (movietitle, year))
+    # UPDATE: not all shows from amazon return year, now will be staged with year if exist
+    # NOTE: iterate over items in execute_json_rpc can be a option to identify the type of content
+    if container_type == 'movies':
+        typeofcontent = 0
+        label = label_with_year(title=label, year=year)
+    elif container_type == 'tvshows':
+        typeofcontent = 1
+        label = label_with_year(title=label, year=year)
     else:
         STR_CHOOSE_CONTENT_TYPE = utils.ADDON.getLocalizedString(32100)
         STR_MOVIE = utils.ADDON.getLocalizedString(32102)
         STR_TV_SHOW = utils.ADDON.getLocalizedString(32101)
-        is_show = xbmcgui.Dialog().yesno(
-            utils.ADDON_NAME, STR_CHOOSE_CONTENT_TYPE, yeslabel=STR_TV_SHOW, nolabel=STR_MOVIE
-        )
-        content_type = 'tvshow' if is_show else 'movie'
+        # Using the Dialog().select method is better as it allows the user to cancel if they want, and we can add more options if needed.
+        typeofcontent = xbmcgui.Dialog().select(STR_CHOOSE_CONTENT_TYPE, ['It is a Movie', 'It is a Show', '[COLOR red][B]Cancel[/B][/COLOR]'])
+        label = label_with_year(title=label, year=year)
 
     # Call corresponding method
-    if content_type == 'movie':
+    if typeofcontent == 0:
         SyncedMenu().sync_single_movie(label, path)
-    elif content_type == 'tvshow':
+    elif typeofcontent == 1:
         SyncedMenu().sync_single_tvshow(label, path)
-
+    elif typeofcontent == -1 or 2:
+        xbmc.sleep(200)
+        utils.notification('Type of content not selected, Try again.')
+        pass
 
 if __name__ == '__main__':
     main()
