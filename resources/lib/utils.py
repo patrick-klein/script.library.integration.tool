47f094b22529e5e2d733c01c6c88a93fd1008b2d
luizoti <luizlazoti@hotmail.com>
2021-06-16 11:40:38 -0300

    update: settings variables moved to __init__

--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -24,27 +24,8 @@ import xbmcgui  # pylint: disable=import-error
 
 from resources.lib.filesystem import mkdir
 
-# Get settings
-ADDON = xbmcaddon.Addon()
-ADDON_ID = ADDON.getAddonInfo('id')
-ADDON_NAME = ADDON.getAddonInfo('name')
-ADDON_PATH = ADDON.getAddonInfo('path')
-ADDON_VERSION = ADDON.getAddonInfo('version')
-AUTO_ADD_MOVIES = ADDON.getSetting('auto_add_movies')
-AUTO_ADD_TVSHOWS = ADDON.getSetting('auto_add_tvshows')
-IN_DEVELOPMENT = ADDON.getSetting('in_development') == 'true'
-RECURSION_LIMIT = int(ADDON.getSetting('recursion_limit'))
-USE_SHOW_ARTWORK = ADDON.getSetting('use_show_artwork') == 'true'
-USING_CUSTOM_MANAGED_FOLDER = ADDON.getSetting('custom_managed_folder') == 'true'
-USING_CUSTOM_METADATA_FOLDER = ADDON.getSetting('custom_metadata_folder') == 'true'
-if USING_CUSTOM_MANAGED_FOLDER:
-    MANAGED_FOLDER = ADDON.getSetting('managed_folder')
-else:
-    MANAGED_FOLDER = xbmcvfs.translatePath('special://userdata/addon_data/{}/'.format(ADDON_ID))
-if USING_CUSTOM_METADATA_FOLDER:
-    METADATA_FOLDER = ADDON.getSetting('metadata_folder')
-else:
-    METADATA_FOLDER = join(MANAGED_FOLDER, 'Metadata')
+from resources import *
+
 
 # Enum values in settings
 NEVER = '0'
