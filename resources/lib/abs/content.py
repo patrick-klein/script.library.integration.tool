f2b54d7095756e8e7141c2fbac63b7879ddddd7a
luizoti <luizlazoti@hotmail.com>
2021-06-28 00:04:40 -0300

    fix: revert docstring quotes

--- resources/lib/abs/content.py
+++ resources/lib/abs/content.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""Module with abs content classes"""
+'''Module with abs content classes'''
 
 import abc
 
 class ABSContentManagerShow(object):
-    """Abstract base class for ContentManager."""
+    '''Abstract base class for ContentManager.'''
     __metaclass__ = abc.ABCMeta
     # TODO: Make rename on add optional in settings
     # TODO: Save original_label, would be able to rename
@@ -28,74 +28,74 @@ class ABSContentManagerShow(object):
 
     @abc.abstractproperty
     def showtitle(self):
-        """Path to the show_dir directory for the item"""
+        '''Path to the show_dir directory for the item'''
 
 
     @abc.abstractproperty
     def show_dir(self):
-        """Path to the show_dir directory for the item"""
+        '''Path to the show_dir directory for the item'''
 
 
     @abc.abstractproperty
     def formedyear(self):
-        """Path to the formedyear directory for the item"""
+        '''Path to the formedyear directory for the item'''
 
 
     @abc.abstractproperty
     def complete_episode_title(self):
-        """Path to the complete_episode_title directory for the item"""
+        '''Path to the complete_episode_title directory for the item'''
 
 
     @abc.abstractproperty
     def file(self):
-        """Path to the episode_title_with_id directory for the item"""
+        '''Path to the episode_title_with_id directory for the item'''
 
 
     @abc.abstractproperty
     def episode_title_with_id(self):
-        """Path to the episode_title_with_id directory for the item"""
+        '''Path to the episode_title_with_id directory for the item'''
 
 
     @abc.abstractproperty
     def episode_nfo(self):
-        """Path to the episode_nfo directory for the item"""
+        '''Path to the episode_nfo directory for the item'''
 
 
     @abc.abstractmethod
     def add_to_library(self):
-        """Add content to the library"""
+        '''Add content to the library'''
         # TODO: add to library using json-rpc
 
 
     @abc.abstractmethod
     def add_to_library_if_metadata(self):
-        """Add content to the library only if it has metadata"""
+        '''Add content to the library only if it has metadata'''
 
 
     @abc.abstractmethod
     def remove_from_library(self):
-        """Remove its content from the library, does NOT change/remove item in database"""
+        '''Remove its content from the library, does NOT change/remove item in database'''
         # TODO: remove from library using json-rpc
 
 
     # @abc.abstractmethod
     # def remove_and_block(self):
-    #     """Remove content from the library, deletes metadata, and adds to blocked list"""
+    #     '''Remove content from the library, deletes metadata, and adds to blocked list'''
 
 
     @abc.abstractmethod
     def create_metadata_item(self):
-        """Add relevent files to metadata folder"""
+        '''Add relevent files to metadata folder'''
 
 
     @abc.abstractmethod
     def rename_using_metadata(self):
-        """Automatically rename using nfo file"""
+        '''Automatically rename using nfo file'''
 
 
     @abc.abstractmethod
     def rename(self, name):
-        """Rename title and files"""
+        '''Rename title and files'''
 
 
     @abc.abstractmethod
@@ -105,7 +105,7 @@ class ABSContentManagerShow(object):
 
     @abc.abstractmethod
     def set_as_staged(self):
-        """Set the item status as staged in database"""
+        '''Set the item status as staged in database'''
 
 
 class ABSContentManagerMovie(object):
@@ -130,96 +130,96 @@ class ABSContentManagerMovie(object):
 
     @abc.abstractproperty
     def link_stream_path(self):
-        """Path to the episode_title_with_id directory for the item"""
+        '''Path to the episode_title_with_id directory for the item'''
 
 
     @abc.abstractproperty
     def movie_title(self):
-        """Path to the show_dir directory for the item"""
+        '''Path to the show_dir directory for the item'''
 
 
     @abc.abstractproperty
     def year(self):
-        """Path to the year for the item"""
+        '''Path to the year for the item'''
 
 
     @abc.abstractproperty
     def formedyear(self):
-        """Path to the formedyear for the item"""
+        '''Path to the formedyear for the item'''
 
 
     @abc.abstractproperty
     def movie_dir(self):
-        """Path to the movie_nfo for the item"""
+        '''Path to the movie_nfo for the item'''
 
 
     @abc.abstractproperty
     def movie_nfo(self):
-        """Path to the movie_nfo for the item"""
+        '''Path to the movie_nfo for the item'''
 
 
     # @abc.abstractproperty
     # def show_dir(self):
-    #     """Path to the show_dir directory for the item"""
+    #     '''Path to the show_dir directory for the item'''
 
 
     # @abc.abstractproperty
     # def formedyear(self):
-    #     """Path to the formedyear directory for the item"""
+    #     '''Path to the formedyear directory for the item'''
 
 
     # @abc.abstractproperty
     # def complete_episode_title(self):
-    #     """Path to the complete_episode_title directory for the item"""
+    #     '''Path to the complete_episode_title directory for the item'''
 
 
     # @abc.abstractproperty
     # def episode_nfo(self):
-    #     """Path to the episode_nfo directory for the item"""
+    #     '''Path to the episode_nfo directory for the item'''
 
 
     @abc.abstractmethod
     def add_to_library(self):
-        """Add content to the library"""
+        '''Add content to the library'''
         # TODO: add to library using json-rpc
 
 
     @abc.abstractmethod
     def add_to_library_if_metadata(self):
-        """Add content to the library only if it has metadata"""
+        '''Add content to the library only if it has metadata'''
 
 
     @abc.abstractmethod
     def remove_from_library(self):
-        """Remove its content from the library, does NOT change/remove item in database"""
+        '''Remove its content from the library, does NOT change/remove item in database'''
         # TODO: remove from library using json-rpc
 
 
     # @abc.abstractmethod
     # def remove_and_block(self):
-    #     """Remove content from the library, deletes metadata, and adds to blocked list"""
+    #     '''Remove content from the library, deletes metadata, and adds to blocked list'''
 
 
     @abc.abstractmethod
     def create_metadata_item(self):
-        """Add relevent files to metadata folder"""
+        '''Add relevent files to metadata folder'''
 
 
     @abc.abstractmethod
     def rename_using_metadata(self):
-        """Automatically rename using nfo file"""
+        '''Automatically rename using nfo file'''
 
 
     @abc.abstractmethod
     def rename(self, name):
-        """Rename title and files"""
+        '''Rename title and files'''
 
 
     @abc.abstractmethod
     def delete(self):
-        """Remove the item from the database"""
+        '''Remove the item from the database'''
 
 
     @abc.abstractmethod
     def set_as_staged(self):
-        """Set the item status as staged in database"""
+        '''Set the item status as staged in database'''
