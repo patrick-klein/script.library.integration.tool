cf718a4dbd5566143d4225727968bafbea024c62
luizoti <luizlazoti@hotmail.com>
2021-06-28 19:58:08 -0300

    update: clean_name moved to /manipulator/__init__

--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -40,35 +40,6 @@ if USING_CUSTOM_METADATA_FOLDER:
 else:
     METADATA_FOLDER = join(MANAGED_FOLDER, 'Metadata')
 
-# TODO: Use combined list on all platforms.  Would need to be combined with version check
-# to re-add all managed items
-MAPPED_STRINGS = [
-    ('.', ''),
-    (':', ''),
-    ('/', ''),
-    ('"', ''),
-    ('$', ''),
-    ('é', 'e'),
-    (' [cc]', ''),
-    ('Part 1', 'Part One'),
-    ('Part 2', 'Part Two'),
-    ('Part 3', 'Part Three'),
-    ('Part 4', 'Part Four'),
-    ('Part 5', 'Part Five'),
-    ('Part 6', 'Part Six'),
-]
-
-if osname == 'nt':
-    MAPPED_STRINGS += [
-        ('?', ''),
-        ('<', ''),
-        ('>', ''),
-        ('\\', ''),
-        ('*', ''),
-        ('|', ''),
-        #('+', ''), (',', ''), (';', ''), ('=', ''), ('[', ''), (']', ''),
-    ]
-
 
 def check_managed_folder():
     '''Checks if the managed folder is configured'''
@@ -123,38 +94,6 @@ def utf8_args(func):
         '''function wrapper'''
         return func(*args, **kwargs)
     return wrapper
-
-
-# TODO: NETFLIX find a way to deal with show with Part 1,
-# Part 2 and etc, now, any part will be a season,
-# maybe a api call with trakt or tvdb to get episode info is a way
-
-# TODO: Giant animes like One Piece is devided in folders with 60 eps,
-# and not splited by season, maybe all eps need to be in the Show dir
-# directly and follow absolute order, to do this is necessary identify this cases.
-
-# TODO: CHRUNCHROLL create a dialog to selec language or
-# use system language to auto select:
-
-def clean_name(title):
-    '''Remove/replace problematic characters/substrings for filenames'''
-    # IDEA: Replace in title directly, not just filename
-
-    # TODO: use this function to remove from Show/episode title on,
-    # Show title
-    # episode number
-    # (Legendado)
-    # (Leg)
-    # (Dub PT)
-    # (French Dub)
-    # (German Dub)
-    # (Portuguese Dub)
-    # (English Dub)
-    # (Spanish Dub)
-    # TODO: Efficient algorithm that removes/replaces in a single
-    for key, val in MAPPED_STRINGS:
-        title = title.replace(key, val)
-    return title
 
 
 def execute_json_rpc(method, _path):
