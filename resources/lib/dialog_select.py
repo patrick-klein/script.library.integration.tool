b604338f33e9d7a031b033458e4fd159ca55f2b9
luizoti <luizlazoti@hotmail.com>
2021-07-07 20:41:25 -0300

    fix: :sparkles: turn bold and same fixes

--- resources/lib/dialog_select.py
+++ resources/lib/dialog_select.py
@@ -1,31 +1,43 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """Custon xbmcgui.Dialog.select."""
-from resources import ADDON_NAME
+
 import xbmcgui  # pylint: disable=import-error
+
+from resources import ADDON_NAME
+
+from resources.lib.utils import bold
+from resources.lib.utils import getstring
 
 
 class Select(xbmcgui.Dialog):
-    def __init__(self, heading=ADDON_NAME):
+    def __init__(self, heading=ADDON_NAME, turnbold=False):
         """CustonDialog __init__."""
         super(Select, self).__init__()
-        self.heading = heading
+        if turnbold:
+            self.heading = bold(heading)
+        else:
+            self.heading = heading
         self.listofitems = None
         self.listofopts = None
         self.extra = None
 
-    def items(self, listofitems, bold=True):
+    def items(self, listofitems, turnbold=True):
         """Add item lines to dialog select."""
-        self.listofitems = listofitems
-        self._list = self.listofitems
+        self.listofitems = [
+            bold(bstr) for bstr in listofitems
+        ] if turnbold else listofitems
+        self._list = listofitems
 
     def extraopts(self, listofopts):
         """Add option lines to dialog select."""
         self.listofopts = listofopts
         self.extra = True
 
-    def show(self, autoclose=False, useDetails=False, preselect=False, back=False, back_value='Backk'):
+    def show(self, autoclose=False, useDetails=False, preselect=False, back=False, back_value=getstring(32011)):
         """Open dialog select with all params."""
+        _type = None
+        selection1 = None
         if self.extra:
             self.listofitems += self.listofopts
         if back:
@@ -37,7 +49,10 @@ class Select(xbmcgui.Dialog):
             preselect=preselect,
             useDetails=useDetails
         )
-        str_opt = self.listofitems[selection]
+        try:
+            str_opt = self._list[selection]
+        except IndexError:
+            str_opt = self.listofitems[selection]
         if selection == -1:
             return None
         elif str_opt == back_value:
@@ -45,9 +60,11 @@ class Select(xbmcgui.Dialog):
         if str_opt in self.listofopts:
             _type = 'opt'
             for i, _str in enumerate(self.listofopts):
+                if _str in str_opt:
+                    selection1 = i
+        elif str_opt in self._list or '[B]' in str_opt:
+            _type = 'item'
+            for i, _str in enumerate(self._list):
                 if _str == str_opt:
                     selection1 = i
-        elif str_opt in self.listofitems:
-            _type = 'item'
-            selection1 = selection
         return {'index': selection, 'index1': selection1, 'str': str_opt, 'type': _type}
