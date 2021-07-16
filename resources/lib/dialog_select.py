b71dd3e4ef1da901ddb116f56e5c84af09b66848
luizoti <luizlazoti@hotmail.com>
2021-07-16 14:51:28 -0300

    feat: :sparkles: dedicated _bold and _getstring functions

--- resources/lib/dialog_select.py
+++ resources/lib/dialog_select.py
@@ -4,10 +4,20 @@
 
 import xbmcgui  # pylint: disable=import-error
 
+from resources import ADDON
 from resources import ADDON_NAME
 
-from resources.lib.utils import bold
-from resources.lib.utils import getstring
+
+def _bold(string):
+    """
+    Return string formated with a bold.
+    """
+    return str('[B]%s[/B]' % (string))
+
+
+def _getstring(string_id):
+    """Shortcut function to return string from String ID."""
+    return str(ADDON.getLocalizedString(string_id))
 
 
 class Select(xbmcgui.Dialog):
@@ -15,7 +25,7 @@ class Select(xbmcgui.Dialog):
         """CustonDialog __init__."""
         super(Select, self).__init__()
         if turnbold:
-            self.heading = bold(heading)
+            self.heading = _bold(heading)
         else:
             self.heading = heading
         self.listofitems = None
@@ -25,7 +35,7 @@ class Select(xbmcgui.Dialog):
     def items(self, listofitems, turnbold=True):
         """Add item lines to dialog select."""
         self.listofitems = [
-            bold(bstr) for bstr in listofitems
+            _bold(bstr) for bstr in listofitems
         ] if turnbold else listofitems
         self._list = listofitems
 
@@ -34,7 +44,7 @@ class Select(xbmcgui.Dialog):
         self.listofopts = listofopts
         self.extra = True
 
-    def show(self, autoclose=False, useDetails=False, preselect=False, back=False, back_value=getstring(32011)):
+    def show(self, autoclose=False, useDetails=False, preselect=False, back=False, back_value=_getstring(32011)):
         """Open dialog select with all params."""
         _type = None
         selection1 = None
@@ -57,12 +67,13 @@ class Select(xbmcgui.Dialog):
             return None
         elif str_opt == back_value:
             return
-        if str_opt in self.listofopts:
-            _type = 'opt'
-            for i, _str in enumerate(self.listofopts):
-                if _str in str_opt:
-                    selection1 = i
-        elif str_opt in self._list or '[B]' in str_opt:
+        if self.listofopts:
+            if str_opt in self.listofopts:
+                _type = 'opt'
+                for i, _str in enumerate(self.listofopts):
+                    if _str in str_opt:
+                        selection1 = i
+        if str_opt in self._list or '[B]' in str_opt:
             _type = 'item'
             for i, _str in enumerate(self._list):
                 if _str == str_opt:
