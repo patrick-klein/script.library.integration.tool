97e35cdcd5f8383a05862a0e5dafc0aaad4ac562
luizoti <luizlazoti@hotmail.com>
2021-09-06 11:26:32 -0300

    updates in settings

--- resources/__init__.py
+++ resources/__init__.py
@@ -12,13 +12,15 @@ ADDON_ID = ADDON.getAddonInfo('id')
 ADDON_NAME = ADDON.getAddonInfo('name')
 ADDON_PATH = ADDON.getAddonInfo('path')
 ADDON_VERSION = ADDON.getAddonInfo('version')
-AUTO_ADD_MOVIES = ADDON.getSettingBool('auto_add_movies')
-AUTO_ADD_TVSHOWS = ADDON.getSettingBool('auto_add_tvshows')
-AUTO_CREATE_NFO_MOVIES = ADDON.getSettingBool('auto_create_nfo_movies')
-AUTO_CREATE_NFO_SHOWS = ADDON.getSettingBool('auto_create_nfo_shows')
+
+AUTO_ADD_MOVIES = ADDON.getSetting('auto_add_movies') == 'true'
+AUTO_ADD_TVSHOWS = ADDON.getSetting('auto_add_tvshows') == 'true'
+AUTO_CREATE_NFO_MOVIES = ADDON.getSetting('auto_create_nfo_movies') == 'true'
+AUTO_CREATE_NFO_SHOWS = ADDON.getSetting('auto_create_nfo_shows') == 'true'
+# USE_SHOW_ARTWORK_SHOW = ADDON.getSetting('use_show_artwork_show') == 'true'
+
 IN_DEVELOPMENT = ADDON.getSetting('in_development') == 'true'
 RECURSION_LIMIT = int(ADDON.getSetting('recursion_limit'))
-# USE_SHOW_ARTWORK_SHOW = ADDON.getSetting('use_show_artwork_show') == 'true'
 
 USING_CUSTOM_MANAGED_FOLDER = ADDON.getSetting('custom_managed_folder') == 'true'
 
